5cb7a1268ecc4c5ca2dff149e5c63243baa520c7
Rafayet Hossain <34092241+rafayethossain@users.noreply.github.com>
2023-01-18 11:19:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,121 @@
-// function to clear the text area
-function clearText() {
-  document.querySelector("#text-area").value = "";
-  document.querySelector("#word-count").innerHTML = "0";
+.body {
+  background: linear-gradient(to right, #82d4bc, #faef58);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  font-family: "Poppins", sans-serif;
 }
 
-// function to count the number of words in real-time as the user types in the text area
-document.querySelector("#text-area").addEventListener("input", function() {
-  var text = this.value;
-  var wordCount = text.split(" ").length;
-  document.querySelector("#word-count").innerHTML = wordCount;
-});
+.word-count-tool {
+  width: 70%;
+  margin: 0 auto;
+  text-align: center;
+  font-family: "Poppins", sans-serif;
+}
 
-// function to show or hide the "how to use" section
-function toggleHowToUse() {
-  var howToUse = document.querySelector(".how-to-use");
-  if (howToUse.style.display === "none") {
-      howToUse.style.display = "block";
-  } else {
-      howToUse.style.display = "none";
+.input-area {
+  margin: 20px 0;
+}
+
+textarea {
+  width: 100%;
+  height: 200px;
+  padding: 15px;
+  border-radius: 10px;
+  border: none;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  font-size: 1.2em;
+  color: #333;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+
+.output-area {
+  margin: 20px 0;
+}
+
+.word-count {
+  font-size: 1.4em;
+}
+
+.character-count-without-spaces {
+  font-size: 1.4em;
+}
+
+.character-count {
+  font-size: 1.4em;
+}
+
+.top-keywords {
+  font-size: 1.4em;
+}
+
+.frequency-of-each-word {
+  font-size: 1.4em;
+}
+
+.how-to-use {
+  margin: 20px 0;
+  display: none;
+}
+
+.footer {
+  margin-top: 30px;
+}
+
+.footer a {
+  color: #333;
+}
+
+.btn {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-size: 1.2em;
+}
+
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
   }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50
+  }
+
+  .top-keywords-block {
+width: 50%;
+margin: 0 auto;
+padding: 20px;
+background-color: #f2f2f2;
+border-radius: 10px;
+box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
 }
 
-// event listener for the "clear" button
-document.querySelector("#clear-btn").addEventListener("click", clearText);
+.top-keywords-block h3 {
+text-align: center;
+margin-bottom: 20px;
+}
+
+.top-keywords-block ul {
+list-style: none;
+padding: 0;
+margin: 0;
+}
+
+.top-keywords-block li {
+display: inline-block;
+margin-right: 10px;
+padding: 5px 10px;
+background-color: #333;
+color: #fff;
+border-radius: 10px;
+font-size: 1.2em;
+cursor: pointer;
+}
+
+.top-keywords-block li:hover {
+background-color: #f12711;
+}
